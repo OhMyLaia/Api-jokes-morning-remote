e7ac670026c85f2208caf3fa04935858facdcd09
OhMyLaia <ohmylaia@live.com>
2025-03-06 12:26:02 +0100

    changed path for images and modules since index.html is now in the root of the project

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -119,16 +119,16 @@ async function showEmojiWeather() {
         }
 
         if (currentClouds < 20) {
-            imgElement.src = "/assets/weather-2-svgrepo-com.svg";
+            imgElement.src = "/public/images/weather-2-svgrepo-com.svg";
 
         } else if (currentClouds > 20 && currentClouds < 60) {
-            imgElement.src = "/assets/weather-sunny-and-cloudy-svgrepo-com.svg";
+            imgElement.src = "/public/images/weather-sunny-and-cloudy-svgrepo-com.svg";
 
         } else if (currentClouds > 60) {
-            imgElement.src = "/assets/weather-symbol-10-svgrepo-com.svg";
+            imgElement.src = "/public/images/weather-symbol-10-svgrepo-com.svg";
 
         } else {
-            imgElement.src = "/assets/weather-severe-alert-symbolic-svgrepo-com.svg";
+            imgElement.src = "/public/images/weather-severe-alert-symbolic-svgrepo-com.svg";
         }
         
 
