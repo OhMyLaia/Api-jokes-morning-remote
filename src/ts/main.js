4d4b3a83868182d5870b3116780d8e4c80bbbdd8
OhMyLaia <ohmylaia@live.com>
2025-03-06 12:41:34 +0100

    changed imgs paths

--- src/ts/main.js
+++ src/ts/main.js
@@ -113,16 +113,16 @@ function showEmojiWeather() {
                 weatherContainer.innerHTML = `| Temperature: ${currentTemperature.toString()}°C | 🥵`;
             }
             if (currentClouds < 20) {
-                imgElement.src = "/public/images/weather-2-svgrepo-com.svg";
+                imgElement.src = "../images/weather-2-svgrepo-com.svg";
             }
             else if (currentClouds > 20 && currentClouds < 60) {
-                imgElement.src = "/public/images/weather-sunny-and-cloudy-svgrepo-com.svg";
+                imgElement.src = "../images/weather-sunny-and-cloudy-svgrepo-com.svg";
             }
             else if (currentClouds > 60) {
-                imgElement.src = "/public/images/weather-symbol-10-svgrepo-com.svg";
+                imgElement.src = "../images/weather-symbol-10-svgrepo-com.svg";
             }
             else {
-                imgElement.src = "/public/images/weather-severe-alert-symbolic-svgrepo-com.svg";
+                imgElement.src = "../images/weather-severe-alert-symbolic-svgrepo-com.svg";
             }
         }
         catch (error) {
